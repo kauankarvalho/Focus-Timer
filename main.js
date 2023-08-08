108cfedb45bc4103c9a3d5c5e0d485c03a3b7f3e
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-08 10:07:05 -0300

    feat: add dark mode functionality to the project

---
+++ main.js
@@ -0,0 +1,6 @@
+const themeButton = document.querySelector(".js-theme-button");
+
+themeButton.addEventListener("click", () => {
+  const html = document.documentElement;
+  html.classList.toggle("dark");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./index.html"],
   safelist: ["play", "pause", "timer", "stop", "speaker-none", "speaker-high"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
