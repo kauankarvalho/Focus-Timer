a3e650cbea1957729fdcca364b0da26581d734c9
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-08 12:36:51 -0300

    feat: add Prettier for code formatting.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const themeButton = document.querySelector(".js-theme-button");
+const themeButton = document.querySelector(".js-theme-button")
 
 themeButton.addEventListener("click", () => {
-  const html = document.documentElement;
-  html.classList.toggle("dark");
-});
+  const html = document.documentElement
+  html.classList.toggle("dark")
+})
