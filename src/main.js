0c17624f9e260ff67f3185690392764e6a2a8b77
kauankarvalho <kauan.karvalho@outlook.com>
2023-08-21 10:59:13 -0300

    fix: function name for focusTimer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { FocusTimer } from "./modules/FocusTimer/index.js"
+import { focusTimer } from "./modules/FocusTimer/index.js"
 import "./modules/ThemeMode/index.js"
 
-FocusTimer()
+focusTimer()
--- src/modules/FocusTimer/index.js
+++ src/modules/FocusTimer/index.js
@@ -1,7 +1,7 @@
 import { updateTimerState, updateDisplay } from "./timer.js"
 import { initEventListeners } from "./events.js"
 
-export function FocusTimer(minutes, seconds) {
+export function focusTimer(minutes, seconds) {
   updateTimerState(minutes, seconds)
   updateDisplay()
   initEventListeners()
